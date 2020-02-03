60319aa974db32e43bdeba7daae03e172f10e3a7
Luyz-Castelo <luyzcastelobranco@hotmail.com>
2020-02-03 17:33:42 -0300

    Upload de Imagens Evento App

--- src/app/_services/evento.service.ts
+++ src/app/_services/evento.service.ts
@@ -14,18 +14,31 @@ export class EventoService {
   getAllEvento(): Observable<Evento[]> {
     return this.http.get<Evento[]>(this.baseURL);
   }
+
   getEventoByTema(tema: string): Observable<Evento[]> {
     return this.http.get<Evento[]>(`${this.baseURL}/getByTema/${tema}`);
   }
+
   getEventoById(id: number): Observable<Evento> {
     return this.http.get<Evento>(`${this.baseURL}/${id}`);
   }
+
+  postUpload(file: File, name: string) {
+    const fileToUpload = <File> file[0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, name);
+
+    return this.http.post(`${this.baseURL}/upload`, formData);
+  }
+
   postEvento(evento: Evento) {
     return this.http.post(this.baseURL, evento);
   }
+
   putEvento(evento: Evento) {
     return this.http.put(`${this.baseURL}/${evento.id}`, evento);
   }
+
   deleteEvento(id: number) {
     return this.http.delete(`${this.baseURL}/${id}`);
   }
--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -27,6 +27,12 @@ export class EventosComponent implements OnInit {
   bodyDeletarEvento = '';
 
   _filtroLista = '';
+  fileNameToUpdate: string;
+
+  dataAtual: string;
+
+  file: File;
+
 
   get filtroLista(): string {
     return this._filtroLista;
@@ -73,11 +79,13 @@ export class EventosComponent implements OnInit {
       });
   }
 
-  editarEvento(template: any, evento: Evento) {
+  editarEvento(evento: Evento, template: any) {
     this.modoSalvar = 'put';
     this.openModal(template);
-    this.evento = evento;
-    this.registerForm.patchValue(evento);
+    this.evento = Object.assign({}, evento);
+    this.fileNameToUpdate = evento.imagemURL.toString();
+    this.evento.imagemURL = '';
+    this.registerForm.patchValue(this.evento);
   }
 
   novoEvento(template: any) {
@@ -120,10 +128,46 @@ export class EventosComponent implements OnInit {
     this.mostrarImagem = !this.mostrarImagem;
   }
 
+  onFileChange(event) {
+    const reader = new FileReader();
+
+    if (event.target.files && event.target.files.length) {
+      this.file = event.target.files;
+      console.log(this.file);
+    }
+  }
+
+  uploadImagem() {
+    if (this.modoSalvar === 'post') {
+     const nomeArquivo = this.evento.imagemURL.split('\\', 3);
+     this.evento.imagemURL = nomeArquivo[2];
+
+     this.eventoService.postUpload(this.file, nomeArquivo[2])
+     .subscribe(
+       () => {
+         this.dataAtual = new Date().getMilliseconds().toString();
+         this.getEventos();
+       }
+     );
+    } else {
+      this.evento.imagemURL = this.fileNameToUpdate;
+      this.eventoService.postUpload(this.file, this.fileNameToUpdate)
+      .subscribe(
+        () => {
+          this.dataAtual = new Date().getMilliseconds().toString();
+          this.getEventos();
+        }
+      );
+    }
+  }
+
   salvarAlteracao(template: any) {
     if (this.registerForm.valid) {
       if (this.modoSalvar === 'post') {
         this.evento = Object.assign({}, this.registerForm.value);
+
+        this.uploadImagem();
+
         this.eventoService.postEvento(this.evento).subscribe(
           (novoEvento: Evento) => {
             template.hide();
@@ -134,7 +178,10 @@ export class EventosComponent implements OnInit {
           }
         );
       } else {
-        this.evento = Object.assign({ id: this.evento.id }, this.registerForm.value);
+        this.evento = Object.assign({id: this.evento.id }, this.registerForm.value);
+
+        this.uploadImagem();
+
         this.eventoService.putEvento(this.evento).subscribe(
           () => {
             template.hide();
