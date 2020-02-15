a0f0431129facfd76e39ed58b39f1566292d461d
César Terceiro <cbterceiro@gmail.com>
2020-02-15 01:04:58 -0300

    Criação de Métodos no Componente EventoEdit

--- src/app/eventos/eventoEdit/eventoEdit.component.ts
+++ src/app/eventos/eventoEdit/eventoEdit.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { EventoService } from 'src/app/_services/evento.service';
-import { BsModalService, BsLocaleService } from 'ngx-bootstrap';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsLocaleService } from 'ngx-bootstrap';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { Evento } from 'src/app/_models/Evento';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-evento-edit',
@@ -12,22 +14,33 @@ import { ToastrService } from 'ngx-toastr';
 export class EventoEditComponent implements OnInit {
 
   titulo = 'Editar Evento';
-  evento = {};
+  evento: Evento = new Evento();
   imagemURL = 'assets/img/upload.png';
   registerForm: FormGroup;
+  fileNameToUpdate: string;
+
+  dataAtual
+
+  get lotes(): FormArray {
+   return <FormArray>this.registerForm.get('lotes');
+  }
+  get redesSociais(): FormArray {
+    return <FormArray>this.registerForm.get('redesSociais');
+   }
 
   constructor(
     private eventoService: EventoService,
-    private modalService: BsModalService,
     private fb: FormBuilder,
     private localeService: BsLocaleService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private router: ActivatedRoute
     ) {
       this.localeService.use('pt-br');
     }
 
     ngOnInit() {
       this.validation();
+      this.carregarEvento();
     }
 
     validation() {
@@ -39,17 +52,73 @@ export class EventoEditComponent implements OnInit {
         qtdPessoas: ['', [Validators.required, Validators.max(120000)]],
         telefone: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
-        lotes: this.fb.group({
-          nome: ['', Validators.required],
-          quantidade: ['', Validators.required],
-          preco: ['', Validators.required],
-          dataInicio: [''],
-          dataFim: ['']
-        }),
-        redesSociais: this.fb.group({
-          nome: ['', Validators.required],
-          url: ['', Validators.required]
-        })
+        lotes: this.fb.array([]),
+        redesSociais:  this.fb.array([])
       });
+    }
+
+    carregarEvento() {
+      const idEvento = +this.router.snapshot.paramMap.get('id');
+      this.eventoService.getEventoById(idEvento).subscribe(
+        (evento: Evento) => {
+          this.evento = Object.assign({}, evento);
+          this.fileNameToUpdate = evento.imagemURL.toString();
+
+          this.imagemURL = `http://localhost:5000/resources/images/${this.evento.imagemURL}?_ts=${this.dataAtual}`
+
+          this.evento.imagemURL = '';
+          this.registerForm.patchValue(this.evento);
+
+          this.evento.lotes.forEach(lote => {
+            this.lotes.push(this.criarLote(lote));
+          });
+          this.evento.redesSociais.forEach(redeSocial => {
+            this.redesSociais.push(this.criarRedeSocial(redeSocial));
+          });
+        }
+      )
+    }
+
+    criarLote(lote: any): FormGroup {
+      return this.fb.group({
+        id: [lote.id],
+        nome: [lote.nome, Validators.required],
+        quantidade: [lote.quantidade, Validators.required],
+        preco: [lote.preco, Validators.required],
+        dataInicio: [lote.dataInicio],
+        dataFim: [lote.dataFim]
+      });
+    }
+
+    criarRedeSocial(redeSocial: any): FormGroup {
+      return this.fb.group({
+        id: [redeSocial.id],
+        nome: [redeSocial.nome, Validators.required],
+        url: [redeSocial.url, Validators.required]
+      });
+    }
+
+    adicionarLote() {
+      this.lotes.push(this.criarLote({ id: 0 }));
+    }
+
+    adicionarRedeSocial() {
+      this.redesSociais.push(this.criarRedeSocial({ id: 0 }));
+    }
+
+    removerLote(id: number) {
+      this.lotes.removeAt(id);
+    }
+
+    removerRedeSocial(id: number) {
+      this.redesSociais.removeAt(id);
+    }
+
+    onFileChange(file: FileList) {
+      const reader = new FileReader();
+
+      reader.onload = (event: any) => this.imagemURL = event.target.result;
+
+      reader.readAsDataURL(file[0]);
     }
   }
